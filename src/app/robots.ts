0632b28bd154b94a1dad95ac330709a36955995e
Z User <z@container>
2026-06-12 18:30:49 +0000

    4e704f58-8592-47c5-9bb6-1e1c1576dc52

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -23,7 +23,7 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/api/'],
       },
     ],
-    sitemap: `${SITE_URL}/sitemap.xml`,
+    sitemap: [`${SITE_URL}/sitemap.xml`, `${SITE_URL}/sitemap-index.xml`],
     host: SITE_URL,
   };
 }
